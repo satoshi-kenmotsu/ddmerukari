ad64a642d2f58ae98ae3475bbfa9936e66231d00
hasiken <kentarou.123.99@gmail.com>
2020-03-20 11:54:12 +0900

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          devise :validatable, password_length: 7..128    
-  validates :nickname, :first_name, :first_name_kana, :last_name, :last_name_kana, :birth_year, :birth_month, :birth_day ,presence: true 
+  validates :nickname, :birth_year, :birth_month, :birth_day ,presence: true 
+  validates :first_name, :last_name,:first_name_kana, :last_name_kana ,presence: true,
+            format: {
+              with: /\A[^ -~｡-ﾟ]+\z/,
+              message: "全角のみで入力して下さい"
+            }
   has_one :address
   
 end
