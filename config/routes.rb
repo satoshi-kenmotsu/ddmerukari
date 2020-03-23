1353e1a6133b6c696dec2a5f398ac7457f0f1828
satoshi-kenmotsu <skem209@gmail.com>
2020-03-23 19:29:51 +0900

    l-single-contaner

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :items, only: [:new]
+  resources :items, only: [:new, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
