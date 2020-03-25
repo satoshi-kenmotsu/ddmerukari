bd1d1055462b2583eb0d19b2874a8bd833fb470e
hasiken <kentarou.123.99@gmail.com>
2020-03-25 20:20:30 +0900

    commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   validates :name, :description, :category, :status, :delivery_charge, :prefecture, :delivery_date, :price, presence: true
+  validates_associated :images
+  validates :images, presence: true
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
 end
