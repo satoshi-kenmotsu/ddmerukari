b9edf913f05d377cd8e554735f6deb79ea25dfec
hasiken <kentarou.123.99@gmail.com>
2020-03-25 16:50:38 +0900

    commit

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
   validates :post_number, :prefecture, :city, :house_number, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
