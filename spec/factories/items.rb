82de1d42fd9c585269cc8c1a3dd97c0f3baaa821
ryu-ok <nfc.63.lddal@gmail.com>
2020-03-31 19:32:29 +0900

    test

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
     
+    name                  {"ねこ"}
+    description           {"動物です。"}
+    status                {"新品、未使用"}
+    price                 {"90000"}
+    category_id           {"1"}
+    prefecture_id         {"2"}
+    delivery_date_id      {"1"}
+    delivery_charge_id    {"2"}
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+  describe '#create' do
+    it "is invalid without a name" do
+      item = build(:item, name: "")
+      item.valid?
+      expect(item.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid without an description" do
+      item = build(:item, description: "")
+      item.valid?
+      expect(item.errors[:description]).to include("can't be blank")
+    end
+
+    it "is invalid without a status" do
+      item = build(:item, status: "")
+      item.valid?
+      expect(item.errors[:status]).to include("can't be blank")
+    end
+
+    it "is invalid without a price" do
+      item = build(:item, price: "")
+      item.valid?
+      expect(item.errors[:price]).to include("can't be blank")
+    end
+
+    it "is invalid without a category_id" do
+      item = build(:item, category_id: "")
+      item.valid?
+      expect(item.errors[:category_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a prefecture_id" do
+      item = build(:item, prefecture_id: "")
+      item.valid?
+      expect(item.errors[:prefecture_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a delivery_date_id" do
+      item = build(:item, delivery_date_id: "")
+      item.valid?
+      expect(item.errors[:delivery_date_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a delivery_date_id" do
+      item = build(:item, delivery_charge_id: "")
+      item.valid?
+      expect(item.errors[:delivery_charge_id]).to include("can't be blank")
+    end
+  end
 end
