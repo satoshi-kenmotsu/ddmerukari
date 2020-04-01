c58dc819c7b7dd629088bb2845db909e8226b9ac
satoshi-kenmotsu <skem209@gmail.com>
2020-04-01 16:03:53 +0900

    商品削除（サーバサイド）

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates :name, :description, :category, :status, :delivery_charge_id, :prefecture_id, :delivery_date_id, :price, presence: true
   validates_associated :images
   validates :images, presence: true
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :category
   extend ActiveHash::Associations::ActiveRecordExtensions
