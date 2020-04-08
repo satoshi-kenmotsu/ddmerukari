91ed2e5845c884f268decd7a334479b4c4728ec8
satoshi-kenmotsu <skem209@gmail.com>
2020-04-08 11:38:33 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_04_02_074038) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -90,9 +89,9 @@ ActiveRecord::Schema.define(version: 2020_04_02_074038) do
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.integer "prefecture_id"
-    t.bigint "seller_id"
     t.integer "delivery_date_id", null: false
     t.integer "delivery_charge_id", null: false
+    t.bigint "seller_id"
     t.bigint "Buyer_id"
     t.index ["Buyer_id"], name: "index_items_on_Buyer_id"
     t.index ["category_id"], name: "index_items_on_category_id"
