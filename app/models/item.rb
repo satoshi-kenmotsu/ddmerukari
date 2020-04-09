478ef97a57e288dce72f97ab80e0d7c920ea4ab1
aka-mo <akamotosan@gmail.com>
2020-04-09 18:02:29 +0900

    commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   #belongs_to :seller, class_name: "User"
   #belongs_to :Buyer, class_name: "User"
+  belongs_to_active_hash :status
   belongs_to_active_hash :delivery_charge
   belongs_to_active_hash :delivery_date
   has_many :comments, dependent: :destroy
---
+++ db/migrate/20200409082301_rename_status_column_to_items.rb
@@ -0,0 +1,6 @@
+class RenameStatusColumnToItems < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :items, :status, :status_id
+
+  end
+end
